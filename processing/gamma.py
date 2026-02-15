8a93b99c92b0347b4d68b074ae96b80612fe5d5b
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2026-02-15 16:22:32 +0530

    Resolving RGB

--- processing/gamma.py
+++ processing/gamma.py
@@ -3,7 +3,7 @@ import cv2
 
 class GammaCorrection:
     def __init__(self, gamma=1.2):
-        self.gamma = gamma
+        self.gamma = max(gamma, 0.1)
 
     def apply(self, image):
         invGamma = 1.0 / self.gamma
--- processing/pipeline.py
+++ processing/pipeline.py
@@ -2,6 +2,7 @@ from .white_balance import WhiteBalance
 from .clahe import CLAHEEnhancer
 from .gamma import GammaCorrection
 from .sharpen import Sharpen
+import numpy as np
 
 class EnhancementPipeline:
 
@@ -12,8 +13,20 @@ class EnhancementPipeline:
         self.sharpen = Sharpen()
 
     def process(self, image):
+
+        image = image.astype("uint8")
+
         image = self.white_balance.apply(image)
+        image = np.clip(image, 0, 255).astype("uint8")
+
         image = self.clahe.apply(image)
+        image = np.clip(image, 0, 255).astype("uint8")
+
         image = self.gamma.apply(image)
+        image = np.clip(image, 0, 255).astype("uint8")
+
         image = self.sharpen.apply(image)
+        image = np.clip(image, 0, 255).astype("uint8")
+
         return image
+
